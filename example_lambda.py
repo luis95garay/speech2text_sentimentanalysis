4d2f7be29486409f0de0d0f9cd1ad3e066f2e82f
Luis González <110075332+luis95garay@users.noreply.github.com>
2023-12-28 10:58:03 -0600

    Create example_lambda.py

---
+++ example_lambda.py
@@ -0,0 +1,11 @@
+import json
+
+def lambda_handler(event, context):
+    response = {
+        'statusCode': 200,
+        'body': 'This is Hello World #2',
+        'headers': {
+            'Content-Type': 'application/json'
+        }
+    }
+    return response
